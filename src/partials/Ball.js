cff29e94c3f06da033892f6c57f8c4f49ac3fafc
Julia Browne <julia.browne@live.ca>
2018-08-03 10:56:52 -0700

    add vectors for ball movement to ball.js file

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -8,20 +8,27 @@ export default class Ball {
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
     this.direction = 1;
+    this.reset();
   }
 
   //this resets the ball after a player scores a "goal"
   reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
+    this.vy = Math.floor(Math.random() * 10 - 5);
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
   render(svg) {
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    //draw ball
     let ball = document.createElementNS(SVG_NS, 'circle');
     ball.setAttributeNS(null, 'fill', 'white');
     ball.setAttributeNS(null, 'r', this.radius);
-    ball.setAttributeNS(null, 'cx', this.boardWidth/2); //x of the centre point
-    ball.setAttributeNS(null, 'cy', this.boardHeight/2); //y of the centre point
+    ball.setAttributeNS(null, 'cx', this.x); //control ball movement
+    ball.setAttributeNS(null, 'cy', this.y); //control ball movement
 
     svg.appendChild(ball);
   }
